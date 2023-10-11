bcb13d212ff387ec3a24218d0c7421b7d7e3ad01
PaviSF <pavithrasoftfruit@gmail.com>
2023-10-11 11:28:10 +0530

    Basic Structure for tabs layout for the home screen

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Tabs } from 'expo-router';
+
+<Tabs>
+  <Tabs.Screen name="dashboard" />
+  <Tabs.Screen name="booking" />
+  <Tabs.Screen name="memberships" />
+  <Tabs.Screen name="profile" />
+</Tabs>;
